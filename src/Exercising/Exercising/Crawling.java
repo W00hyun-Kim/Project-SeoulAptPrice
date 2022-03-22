d8d27a1da7ef79353aa075d441b4307842f15ecd
Woohyun Kim <woohyun9509@gmail.com>
2022-03-23 07:55:07 +0900

    07:54

--- src/Exercising/Exercising/Crawling.java
+++ src/Exercising/Exercising/Crawling.java
@@ -26,14 +26,12 @@ public class Crawling {
 				
 		try{
 			
-			String category = "시군구,번지,본번,부번,단지명,전용면적(㎡),계약년월,계약일,거래금액(만원),층,건축년도,도로명,해제사유발생일,"
-					+ "거래유형,중개사소재지,음식점(상호명),소요시간,거리,카페(상호명),소요시간,거리,쇼핑(상호명),소요시간,거리,숙박(상호명),소요시간,거리,병원(상호명),소요시간,거리"
-					+ "주유소(상호명),소요시간,거리,마트슈퍼(상호명),소요시간,거리,편의점(상호명),소요시간,거리,생활편의(상호명),소요시간,거리,명소,소요시간,거리,체육시설(상호명),소요시간,거리"
-					+ "영화공연(상호명),소요시간,거리,관공서,소요시간,거리";
-			fw.csv(category);
+//			String category = "시군구,번지,본번,부번,단지명,전용면적(㎡),계약년월,계약일,거래금액(만원),층,건축년도,도로명,해제사유발생일,"
+//					+ "거래유형,중개사소재지,명소,시간,거리";
+//			fw.csv(category);
 			
 			String str [] = null;
-			for (int i = 1; i <=1; i++) {		//split.list.size();
+			for (int i = 302; i <=1107; i++) {		//split.list.size();
 				System.setProperty(DRIVER_ID, DRIVER_PATH);
 				ChromeOptions chromeOptions = new ChromeOptions(); 
 //				chromeOptions.addArguments("--headless"); 
@@ -64,21 +62,23 @@ public class Crawling {
 						
 					try {Thread.sleep(2000);} catch (InterruptedException e) {} 
 					
-					for (int j = 1; j <=14; j++) {
+					
+//					ArrayList<String> categ = new ArrayList<String>();
+//					for (int j = 1; j <=14; j++) {
 						
-						driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div/div/div/div/a["+j+"]")).click();
+						driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div/div/div/div/a[11]")).click();
 						
 						try {Thread.sleep(2000);} catch (InterruptedException e) {} 
 						driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[5]/label[2]")).click();				
 						try {Thread.sleep(2000);} catch (InterruptedException e) {} 
-						//����� ��ü� �̸�
+						
 						String name =driver.findElement(By.xpath("/html/body/div[4]/div[2]/ul/li[1]/div[1]/a/div/strong")).getText();
 						try {Thread.sleep(2000);} catch (InterruptedException e) {} 
 						
 						driver.findElement(By.xpath("/html/body/div[4]/div[2]/ul/li[1]/div[2]/a[2]")).click();			
 						try {Thread.sleep(2000);} catch (InterruptedException e) {} 
 						driver.findElement(By.xpath("/html/body/div[2]/div[1]/section[1]/div/div/div/div[2]/div[2]/div[1]/button[1]/span[2]")).click();
-						try {Thread.sleep(2000);} catch (InterruptedException e) {} 
+						try {Thread.sleep(4000);} catch (InterruptedException e) {} 
 						
 						driver.findElement(By.xpath("html/body/div[2]/div[1]/section[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/div[2]/div[1]/div/input")).clear();
 						try {Thread.sleep(2000);} catch (InterruptedException e) {} 
@@ -101,7 +101,20 @@ public class Crawling {
 						str[i]=add;
 						String result = ""+split.list.get(i)+add;
 						fw.csv(result);
-					}					
+//						categ.add(add);
+//						driver.navigate().back();
+//						driver.navigate().back();
+//						
+//					}					
+//					str = new String[split.address.size()];
+//					
+//					String info = null;
+//					for(int k=0; k<categ.size(); k++) {
+//						info =""+categ.get(k);
+//					}					
+//					str[i]=info;
+//					String result = ""+split.list.get(i)+info;
+//					fw.csv(result);
 				} 
 					
 				driver.quit();
